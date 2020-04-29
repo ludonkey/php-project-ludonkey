056796f1eba6375531c06af03e7ab6ea710bcfcc
equesada <equesada@ludonkey.com>
2020-04-29 19:56:50 +0200

    [FEATURE] search by language

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Entity\Code;
+use Entity\Language;
 use ludk\Persistence\ORM;
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -9,8 +10,19 @@ $orm = new ORM(__DIR__ . '/../Resources');
 $codeRepo = $orm->getRepository(Code::class);
 
 $items = array();
+
 if (isset($_GET['search'])) {
-    $items = $codeRepo->findBy(array("content" => $_GET['search']));
+    $strToSearch = $_GET['search'];
+    if (strpos($strToSearch, "_") === 0) {
+        $languageName = substr($strToSearch, 1);
+        $languageRepo = $orm->getRepository(Language::class);
+        $languages = $languageRepo->findBy(array("name" => $languageName));
+        if (count($languages) == 1) {
+            $items = $codeRepo->findBy(array("language" => $languages[0]->id));
+        }
+    } else {
+        $items = $codeRepo->findBy(array("content" => $strToSearch));
+    }
 } else {
     $items = $codeRepo->findAll();
 }
@@ -26,7 +38,7 @@ if (isset($_GET['search'])) {
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/styles/dracula.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?time=<?= time(); ?>">
 </head>
 
 <body>
@@ -44,7 +56,8 @@ if (isset($_GET['search'])) {
 
                 </ul>
                 <form class="form-inline my-2 my-md-0" action="/search" id="search-form">
-                    <input class="form-control" type="text" name="search" placeholder="Search" aria-label="Search">
+                    <input class="form-control" type="text" name="search" placeholder="Search" aria-label="Search"
+                        value="<?= $_GET['search'] ?? ""; ?>">
                 </form>
                 <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true" id="login-btn">Log
                     in</a>
@@ -58,6 +71,9 @@ if (isset($_GET['search'])) {
         <div class="row masonry-grid">
             <div class="col-md-6 col-lg-4 masonry-column">
                 <?php
+                if (count($items) == 0) {
+                    echo '<p class="no-result">No result.</p>';
+                }
                 $oneColumnItemNumber = ceil(count($items) / 3);
                 $itemNumber = 0;
                 foreach ($items as $oneCode) {
@@ -72,9 +88,10 @@ if (isset($_GET['search'])) {
                                 <path
                                     d="M15 6.675l-1.8-.6c-.2-.1-.3-.3-.2-.4l.9-1.7c.6-1.2-.7-2.5-1.9-1.9l-1.7.9c-.1.1-.3 0-.4-.2l-.6-1.8c-.4-1.3-2.2-1.3-2.6 0l-.6 1.8c-.1.2-.3.3-.4.2l-1.7-.9c-1.2-.6-2.5.7-1.9 1.9l.9 1.7c.1.1 0 .3-.2.4l-1.8.6c-1.3.4-1.3 2.3 0 2.7l1.8.6c.2 0 .3.2.2.3l-.9 1.7c-.6 1.2.7 2.5 1.9 1.9l1.7-.9c.2-.1.4 0 .4.2l.6 1.8c.4 1.3 2.3 1.3 2.7 0l.6-1.8c.1-.2.3-.3.4-.2l1.7.9c1.2.6 2.5-.7 1.9-1.9l-1-1.7c-.1-.2 0-.4.2-.4l1.8-.6c1.3-.4 1.3-2.2 0-2.6zm-7 3.7c-1.3 0-2.4-1.1-2.4-2.4 0-1.3 1.1-2.4 2.4-2.4 1.3 0 2.4 1.1 2.4 2.4 0 1.3-1.1 2.4-2.4 2.4z"
                                     fill="#CCC"></path>
-                            </svg> <?= $oneCode->title ?> <span
-                                class="badge badge-secondary lg-<?= $oneCode->language->name ?>">
-                                <?= strtoupper($oneCode->language->name) ?></span></h5>
+                            </svg> <?= $oneCode->title ?> <a
+                                href="/search?search=_<?= $oneCode->language->name ?>"><span
+                                    class="badge badge-secondary lg-<?= $oneCode->language->name ?>">
+                                    <?= strtoupper($oneCode->language->name) ?></span></a></h5>
                         <pre><code class="<?= $oneCode->language->name ?>"><?= htmlentities($oneCode->content) ?></code></pre>
                         <hr />
                         <p class="card-text">
