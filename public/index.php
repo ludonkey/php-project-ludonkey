529c07a51bd0d3bf335fc5b3edfccf96c986d1d0
Eric Quesada <equesada@ludonkey.com>
2020-06-03 11:05:13 +0200

    [FEATURE] register

--- public/index.php
+++ public/index.php
@@ -18,10 +18,36 @@ $languageRepo = $orm->getRepository(Language::class);
 $action = $_GET["action"] ?? "display";
 switch ($action) {
     case 'register':
+        if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['passwordRetype'])) {
+            $errorMsg = NULL;
+            $users = $userRepo->findBy(array("nickname" => $_POST['username']));
+            if (count($users) > 0) {
+                $errorMsg = "Nickname already used.";
+            } else if ($_POST['password'] != $_POST['passwordRetype']) {
+                $errorMsg = "Passwords are not the same.";
+            } else if (strlen(trim($_POST['password'])) < 8) {
+                $errorMsg = "Your password should have at least 8 characters.";
+            } else if (strlen(trim($_POST['username'])) < 4) {
+                $errorMsg = "Your nickame should have at least 4 characters.";
+            }
+            if ($errorMsg) {
+                include "../templates/register.php";
+            } else {
+                $newUser = new User();
+                $newUser->nickname = $_POST['username'];
+                $newUser->password = $_POST['password'];
+                $manager->persist($newUser);
+                $manager->flush();
+                $_SESSION['user'] = $newUser;
+                header('Location:/?action=display');
+            }
+        } else {
+            include "../templates/register.php";
+        }
         break;
     case 'logout':
         if (isset($_SESSION['user'])) {
-            session_destroy();
+            unset($_SESSION['user']);
         }
         header('Location:/?action=display');
         break;
