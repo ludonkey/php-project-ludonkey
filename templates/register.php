af5c6b6fb72cad17616dd7b47acd556b3890837c
Eric Quesada <equesada@ludonkey.com>
2020-06-03 11:05:13 +0200

    [FEATURE] register

--- templates/register.php
+++ templates/register.php
@@ -14,23 +14,25 @@ include "inc/head.php";
         <div class="row">
             <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
                 <div id="login">
-                    <form class="form-signin" action="/?action=login" method="POST">
+                    <form class="form-signin" action="/?action=register" method="POST">
                         <p>
                             <?php
                             if (isset($errorMsg)) {
                                 echo "<div class='alert alert-warning' role='alert'>$errorMsg</div>";
                             }
                             ?>
-                            <h5>Welcome Back</h5>
+                            <h5>Register</h5>
                         </p>
                         <p>
                             <label>Your username:</label>
                             <input type="text" name="username" class="form-control" placeholder="Username">
                             <label>Your password:</label>
                             <input type="password" name="password" class="form-control" placeholder="Password">
+                            <label>Retype your password:</label>
+                            <input type="password" name="passwordRetype" class="form-control" placeholder="Retype Password">
                         </p>
                         <p>
-                            <button type="submit" class="btn btn-lg btn-primary btn-block">Login</button>
+                            <button type="submit" class="btn btn-lg btn-primary btn-block">Register</button>
                         </p>
                     </form>
                 </div>
